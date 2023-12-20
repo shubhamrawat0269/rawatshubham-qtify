ed26e8abff26bf76fb140518ce04b15af62992dc
shubham Rawat <rawatshubam269@gmail.com>
2023-12-20 09:36:07 +0530

    Hero Section Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./_global.css";
 import Navbar from "./Components/Navbar/Navbar";
+import HeroSection from "./Components/HeroSection/HeroSection";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 };
---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./HeroSection.module.css";
+
+const HeroSection = () => {
+  return (
+    <div className={styles.heroSection}>
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+
+      <div>
+        <img src="assets/hero.png" alt="heroImg" />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
