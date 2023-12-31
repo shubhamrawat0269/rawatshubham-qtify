3ba863d2d2ce40904fc7d6e3ef0f3c4d125865e7
shubham Rawat <rawatshubam269@gmail.com>
2023-12-31 12:29:39 +0530

    conversion of showAll btn

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -14,7 +14,7 @@ const Section = ({ category, data }) => {
           <div>
             <div className={styles.section}>
               <h3>{category}</h3>
-              {!isTopAlbumCollapse ? (
+              {isTopAlbumCollapse ? (
                 <button
                   className="btn"
                   onClick={() => setTopAlbumCollapse(!isTopAlbumCollapse)}
@@ -60,7 +60,7 @@ const Section = ({ category, data }) => {
           <div style={{ borderTop: `1px solid #34C94B`, paddingTop: 5 }}>
             <div className={styles.section}>
               <h3>{category}</h3>
-              {!isNewAlbumCollapse ? (
+              {isNewAlbumCollapse ? (
                 <button
                   className="btn"
                   onClick={() => setNewAlbumCollapse(!isNewAlbumCollapse)}
