e458e2ac89033a499612055563365babbd76d763
shubham Rawat <rawatshubam269@gmail.com>
2024-01-02 12:12:12 +0530

    call Songs Api Data -- aded

--- src/Components/Card/CardGrid.js
+++ src/Components/Card/CardGrid.js
@@ -1,3 +1,52 @@
+// import React, { useEffect, useState } from "react";
+// import styles from "./Card.module.css";
+// import Section from "../Section/Section";
+// import axios from "axios";
+// const CardGrid = () => {
+//   const [cardData, setCardData] = useState([]);
+//   const [newAlbumData, setNewAlbumData] = useState([]);
+
+//   const endpoint = `https://qtify-backend-labs.crio.do`;
+
+//   // fetching Card Data
+//   // follows, image, id, title, description, slug, songs : [array] ..
+
+//   const getTopAlbumData = async () => {
+//     try {
+//       const response = await axios.get(`${endpoint}/albums/top`);
+//       setCardData(response.data);
+//     } catch (error) {
+//       console.log(error);
+//       return null;
+//     }
+//   };
+
+//   const getNewAlbumData = async () => {
+//     try {
+//       const response = await axios.get(`${endpoint}/albums/new`);
+//       setNewAlbumData(response.data);
+//     } catch (error) {
+//       console.log(error);
+//       return null;
+//     }
+//   };
+
+//   useEffect(() => {
+//     getTopAlbumData();
+//     getNewAlbumData();
+//     /* eslint-disable react-hooks/exhaustive-deps */
+//   }, []);
+
+//   return (
+//     <div className={styles.cardContainer}>
+//       <Section category="Top Albums" data={cardData} />
+//       <Section category="New Albums" data={newAlbumData} />
+//     </div>
+//   );
+// };
+
+// export default CardGrid;
+
 import React, { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 import Section from "../Section/Section";
@@ -14,6 +63,7 @@ const CardGrid = () => {
   const getTopAlbumData = async () => {
     try {
       const response = await axios.get(`${endpoint}/albums/top`);
+      let songsResponse = await axios.get(`${endpoint}/songs`);
       setCardData(response.data);
     } catch (error) {
       console.log(error);
@@ -24,6 +74,7 @@ const CardGrid = () => {
   const getNewAlbumData = async () => {
     try {
       const response = await axios.get(`${endpoint}/albums/new`);
+      let songsResponse = await axios.get(`${endpoint}/songs`);
       setNewAlbumData(response.data);
     } catch (error) {
       console.log(error);
